7a8ed477020049aea13eb3c65b706e8cd6f67276
Hieu Pham <104843642+chickenbrain247@users.noreply.github.com>
2024-03-31 23:52:18 +0700

    fix

--- app/src/main/java/com/example/classroom02/Fragment/UserInfoFragment.java
+++ app/src/main/java/com/example/classroom02/Fragment/UserInfoFragment.java
@@ -1,20 +1,22 @@
 package com.example.classroom02.Fragment;
 
-
-import android.app.Fragment;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
+import com.example.classroom02.R;
+
 
 public class UserInfoFragment extends Fragment {
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.user_info, container, false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.user_info, container, false);
     }
 }
