ab000d8a5fa80639428f189ba756d19c9c092ed1
Hieu Pham <104843642+chickenbrain247@users.noreply.github.com>
2024-03-31 23:43:16 +0700

    accounttfragment

---
+++ app/src/main/java/com/example/classroom02/Fragment/UserInfoFragment.java
@@ -0,0 +1,20 @@
+package com.example.classroom02.Fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class UserInfoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.user_info, container, false);
+        return view;
+    }
+}
