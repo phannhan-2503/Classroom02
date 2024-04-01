6ffc271d54c3a2a50034ce7956ee0b9622fa8b1c
tungd <tungduong2288@gmail.com>
2024-04-01 14:10:23 +0700

    Chức năng tạo Class

--- app/src/main/java/com/example/classroom02/Adapter/Classroom.java
+++ app/src/main/java/com/example/classroom02/Adapter/Classroom.java
@@ -3,6 +3,17 @@ package com.example.classroom02.Adapter;
 public class Classroom {
     private String imageUrl;
     private String name;
+    private String part;
+    private String room;
+    private String theme;
+
+    public Classroom(String imageUrl, String name, String part, String room, String theme) {
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.part = part;
+        this.room = room;
+        this.theme = theme;
+    }
 
     public Classroom(String imageUrl, String name) {
         this.imageUrl = imageUrl;
@@ -23,6 +34,30 @@ public class Classroom {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public void setPart(String part) {
+        this.part = part;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
     }
 }
 
