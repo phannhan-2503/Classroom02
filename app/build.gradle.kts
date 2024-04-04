9296350fe4bf27b8dce43348d2dcccfaf5f6da8c
Hieu Pham <104843642+chickenbrain247@users.noreply.github.com>
2024-04-04 23:53:20 +0700

    login,register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -52,7 +53,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
 
     implementation("com.squareup.picasso:picasso:2.71828")
-    
+
     implementation ("androidx.appcompat:appcompat:1.3.0")
     implementation ("androidx.fragment:fragment:1.4.0")
 
--- app/src/main/java/com/example/classroom02/MainActivity.java
+++ app/src/main/java/com/example/classroom02/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.home);
-
         }
     }
     @Override
